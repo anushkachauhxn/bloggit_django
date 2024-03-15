6c72e76f3c5a9252620858d7493cfb082bd79a2f
Anushka Chauhan <anushkachauhan1503@gmail.com>
2024-03-15 23:45:59 +0530

    context + template inheritance

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'ABC',
+        'title': "Blog Post 1",
+        'content': "contentttt",
+        'date_posted': "August 28, 2011"
+    },
+    {
+        'author': 'XYZ',
+        'title': "Blog Post 2",
+        'content': "tttt",
+        'date_posted': "August 29, 2011"
+    },
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    template_name = 'blog/home.html'
+    context = {
+        'posts': posts
+    }
+    return render(request, template_name, context)
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    template_name = 'blog/about.html'
+    context = {
+        'title': 'About'
+    }
+    return render(request, template_name, context)
