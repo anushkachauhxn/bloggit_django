cf15a6b60eff15fce132bfdfb2218c57ffc4c392
Anushka Chauhan <anushkachauhan1503@gmail.com>
2024-03-16 19:18:52 +0530

    register page form

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+def register(request):
+    template_name = 'users/register.html'
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
+    else:
+        form = UserCreationForm()
+    return render(request, template_name, { 'form': form })
