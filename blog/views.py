ae8f768a310266a05d7a07b5462a61555f9e623c
Anushka Chauhan <anushkachauhan1503@gmail.com>
2024-03-17 10:56:47 +0530

    post create view + get absolute url

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView
+)
 from .models import Post
-
-
-def home(request):
-    template_name = 'blog/home.html'
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, template_name, context)
 
 
 class PostListView(ListView):
@@ -20,6 +17,15 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
